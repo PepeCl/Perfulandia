cbd8c7f058cb3706b08f7b3a65995d85c313a8e8
Bastian Becerra <bastianbecerrabasoalto@gmail.com>
2025-05-25 15:13:05 -0400

    Agregado de Post Como medoto. Tambien hubieron modificaciones comunes de cada archivo

--- src/main/java/com/perfulandia_spa/cl/perfulandia_spa/controller/UsuarioController.java
+++ src/main/java/com/perfulandia_spa/cl/perfulandia_spa/controller/UsuarioController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,14 @@ public class UsuarioController {
             return new ResponseEntity<>(usuarios, HttpStatus.OK);
         }
     }
+
+
+    @PostMapping
+    public ResponseEntity<Usuario> guardar(@RequestBody Usuario usuario){
+        Usuario usuarioNuevo = usuarioService.save(usuario);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(usuarioNuevo);
+    }
+
+    
 
 }
